c252fbda437fa3266810a9f7d400109f2efb12c8
gwyn <gwyn.evans@gmail.com>
2016-05-07 21:10:40 +0100

    Add Countdown title

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -67,6 +67,7 @@ var Countdown = React.createClass({
 
     return (
       <div>
+        <h3 className="page-title">Countdown App</h3>
         <Clock totalSeconds={count}/>
         {renderControlArea()}
       </div>
